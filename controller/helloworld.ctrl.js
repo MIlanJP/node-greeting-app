89da2fa80823a396640bf05ff13c7ad69cb79b5e
kyoolbaba <kyoolbaba@gmail.com>
2020-08-11 19:57:29 +0530

    Added negative test case for helloworld message

--- controller/helloworld.ctrl.js
+++ controller/helloworld.ctrl.js
@@ -1,6 +1,13 @@
 const Helloworld= require('../services/helloworld.srvs')
+
+// creating HelloWorld Object
 const hw=new Helloworld();
 
+/**
+ * @description This function has responsibility to return hello World message when called 
+ * @param {Http Request Object} req 
+ * @param {Http Response Object} res 
+ */
 exports.getHelloWorld=(req,res)=>{
     const helloworldmessage=hw.returnHelloWorld();
     res.setHeader('Content-Type','application/json');
